a21ce8bd6bddf518641da63a076d370cf186f047
DemiTavares908 <59691843+DemiTavares908@users.noreply.github.com>
2020-01-21 22:46:42 +0100

    Janky Menu
    Let's hope it works

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuThingy : MonoBehaviour
+{
+    public OVRInput.Controller controller = OVRInput.Controller.None;
+    public GameObject Menu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(OVRInput.GetDown(OVRInput.Button.Start, controller) == true)
+        {
+            this.gameObject.SetActive(true);
+        }else
+        {
+            this.gameObject.SetActive(false);
+        }
+        
+    }
+}
